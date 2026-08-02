ca4ffea564cbdb1b89fcfc27848ecb930d58c190
Michael Song <67977729+michaelsong06@users.noreply.github.com>
2026-08-02 15:59:26 +0100

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        unordered_map<int, int> s;
+        for (int i = 0; i < n; ++i) {
+            if (s.count(target - nums[i])) {
+                return {s[target - nums[i]], i};
+            }
+            s.insert({nums[i], i});
+        }
+        return {};
+    }
+};
