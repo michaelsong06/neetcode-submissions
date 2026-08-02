fd5e0a12e8b5d39852e348e837a2ad1def24b69d
Michael Song <67977729+michaelsong06@users.noreply.github.com>
2026-08-02 16:31:13 +0100

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        vector<int> pref(n, 1);
+        vector<int> suff(n, 1);
+
+        for (int i = 1; i < n; ++i) {
+            pref[i] = nums[i - 1] * pref[i - 1];
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            suff[i] = nums[i + 1] * suff[i + 1];
+        }
+
+        vector<int> out(n);
+        for (int i = 0; i < n; ++i) {
+            out[i] = pref[i] * suff[i];
+        }
+
+        return out;
+    }
+};
