514c50ec562a3e269d31b95098eef331f56c2db2
Michael Song <67977729+michaelsong06@users.noreply.github.com>
2026-09-06 16:08:26 +0100

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int dp_f(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; ++i) {
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[n - 1];
+    }
+public:
+    int rob(vector<int>& nums) {
+
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0], nums[1]);
+        
+        vector<int> nums1(nums.begin() + 1, nums.end());
+        vector<int> nums2(nums.begin(), nums.end() - 1);
+        return max(dp_f(nums1), dp_f(nums2));
+    }
+};
