ca4eaa89b434db4b3d49d79d84cfdfd6d67b1857
Michael Song <67977729+michaelsong06@users.noreply.github.com>
2026-09-15 23:46:33 +0100

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0)); // i, owns
+
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i][0] = max(dp[i+1][1] - prices[i], dp[i+1][0]);
+            dp[i][1] = max((i + 2 < n) ? dp[i+2][0] + prices[i] : prices[i], dp[i+1][1]);
+        }
+
+        return dp[0][0];
+
+    }
+};
