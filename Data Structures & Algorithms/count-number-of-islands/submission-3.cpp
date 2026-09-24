cf86bd57bb6c475a8125cd770cec2c139e4aa991
Michael Song <67977729+michaelsong06@users.noreply.github.com>
2026-09-24 22:29:15 +0100

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.cpp
@@ -0,0 +1,83 @@
+class Solution {
+private:
+    int dfsImpl(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        int numIslands = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (visited[i][j]) continue;
+                if (grid[i][j] == '0') continue;
+                dfs(grid, m, n, visited, i, j);
+                numIslands++;
+            }
+        }
+
+        return numIslands;
+    }
+    void dfs(vector<vector<char>>& grid, int& m, int& n, vector<vector<bool>>& visited, int i, int j) {
+        if (grid[i][j] == '0') return;
+        if (visited[i][j]) return;
+        
+        visited[i][j] = true;
+
+        if (i > 0) dfs(grid, m, n, visited, i - 1, j);
+        if (i < m - 1) dfs(grid, m, n, visited, i + 1, j);
+        if (j > 0) dfs(grid, m, n, visited, i, j - 1);
+        if (j < n - 1) dfs(grid, m, n, visited, i, j + 1);
+    }
+
+    int bfsImpl(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        int numIslands = 0;
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                
+                if (grid[i][j] == '0') continue;
+                if (visited[i][j]) continue;
+
+                q.emplace(i, j);
+                visited[i][j] = true;
+
+                while (!q.empty()) {
+                    auto cell = q.front(); q.pop();
+                    int y = cell.first, x = cell.second;
+
+                    if (grid[y][x] == '0') continue;
+
+                    if (y > 0 && !visited[y-1][x]) {
+                        q.emplace(y-1, x);
+                        visited[y-1][x] = true;
+                    }
+                    if (y < m - 1 && !visited[y+1][x]) {
+                        q.emplace(y+1, x);
+                        visited[y+1][x] = true;
+                    }
+                    if (x > 0 && !visited[y][x-1]) {
+                        q.emplace(y, x-1);
+                        visited[y][x-1] = true;
+                    }
+                    if (x < n - 1 && !visited[y][x+1]) {
+                        q.emplace(y, x+1);
+                        visited[y][x+1] = true;
+                    }
+                }
+
+                numIslands++;
+            }
+        }
+
+        return numIslands;
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        return bfsImpl(grid);
+    }
+};
